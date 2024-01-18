8660bdc1e6c80ec85a4a8357a7ad03495bfed6b4
Mattegoat <matteo.georges74@hotmail.fr>
2024-01-18 22:31:55 +0100

    feat: add repo links

---
+++ docs/src/components/GettingStarted.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const productList = [
+  {
+    title: "Price Feeds",
+    description:
+      "Provable data for DeFi. The highest quality data for your dapp.",
+    button: "integrate now",
+    link: "/Resources/Cairo%201/data-feeds/consuming-data",
+  },
+  {
+    title: "Computational feeds",
+    description:
+      "Provable feeds built with our price feeds. Volatility, Yield curve and more.",
+    button: "integrate now",
+    link: "/Resources/Cairo%201/computational-feeds/what-are-computational-feeds",
+  },
+  {
+    title: "PragmAPI",
+    description: "The highest quality data for the lowest latency.",
+    button: "integrate now",
+    link: "/Resources/PragmApi/overview",
+  },
+  {
+    title: "VRF",
+    description: "Verifiable, tamper-proof randomness available onchain.",
+    button: "integrate now",
+    link: "/Resources/Cairo%201/randomness/randomness",
+  },
+];
+
+export default function GettingStarted() {
+  return (
+    <div className={styles.startWrapper}>
+      {productList.map((product, index) => (
+        <div key={index} className={styles.productBox}>
+          <h4>{product.title}</h4>
+          <div className={styles.descriptionStart}>{product.description}</div>
+          <a className={styles.buttons} href={product.link}>
+            Integrate Now
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
