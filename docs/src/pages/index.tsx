0d6ab29ff9d21b45b76c1821f426d561c46f3d58
Mattegoat <matteo.georges74@hotmail.fr>
2024-01-21 19:46:35 +0100

    feat: add footer

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -29,6 +29,7 @@ import GettingStarted from "../components/GettingStarted";
 import DeveloperLink from "../components/DeveloperLink";
 import Help from "../components/Help";
 import SocialWrapper from "../components/SocialWrapper";
+import Footer from "../components/Footer";
 
 export const actions = [
   {
@@ -51,21 +52,6 @@ export const actions = [
     icon: QuestionMarkCircleIcon,
     to: "./howItWorks/architecture",
     text: `New to Validity Rollups? Get started right now and explore the key concepts using high computation.`,
-  },
-];
-
-export const Guides = [];
-
-export const quick = [
-  {
-    title: "Smart Contracts",
-    text: "Take a look at smart conracts",
-    to: "./protocol/reference/smart-contracts",
-  },
-  {
-    title: "Whitepaper",
-    text: "Read the Pragma whitepaper",
-    to: "",
   },
 ];
 
@@ -148,31 +134,6 @@ const CenterCard = styled(Card)`
 
   p {
     margin-bottom: 0px;
-  }
-`;
-
-const Footer = styled(Card)`
-  width: 90%;
-  position: relative;
-  min-height: 100px;
-  border-radius: 10px;
-  border: 1px solid var(--ifm-color-emphasis-300);
-  cursor: unset;
-  overflow: hidden;
-  padding: unset;
-  margin: auto;
-  margin-top: 80px;
-  margin-bottom: 20px;
-  font-size: 20px;
-  &:hover {
-    border: 1px solid var(--ifm-color-emphasis-300);
-    cursor: unset;
-  }
-  @media (max-width: 960px) {
-    width: 80%;
-  }
-  @media (max-width: 640px) {
-    width: 80%;
   }
 `;
 
@@ -300,125 +261,7 @@ export default function Home() {
         <BoxContainer>
           <SocialWrapper />
         </BoxContainer>
-        <Row>
-          <h2>Stay in touch</h2>
-          <h2></h2>
-          <h2></h2>
-
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://discord.gg/GFFymAvNQ6"}
-          >
-            <CenterCard>
-              <Discord style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Discord</h3>
-                <p>Hop in to the community on Discord.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://twitter.com/PragmaOracle"}
-          >
-            <CenterCard>
-              <Twitter style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Twitter</h3>
-                <p>Follow Pragma on Twitter</p>
-              </div>
-            </CenterCard>
-          </Link>
-
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://github.com/Astraly-Labs"}
-          >
-            <CenterCard>
-              <StyledGithubIcon>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 120.78 117.79"
-                  style={{ width: "48px" }}
-                >
-                  <defs></defs>
-                  <g id="Layer_2" data-name="Layer 2">
-                    <g id="Layer_1-2" data-name="Layer 1">
-                      <path
-                        class="cls-1"
-                        d="M60.39,0A60.39,60.39,0,0,0,41.3,117.69c3,.56,4.12-1.31,4.12-2.91,0-1.44-.05-6.19-.08-11.24C28.54,107.19,25,96.42,25,96.42c-2.75-7-6.71-8.84-6.71-8.84-5.48-3.75.41-3.67.41-3.67,6.07.43,9.26,6.22,9.26,6.22,5.39,9.23,14.13,6.57,17.57,5,.55-3.9,2.11-6.56,3.84-8.07C36,85.55,21.85,80.37,21.85,57.23A23.35,23.35,0,0,1,28.08,41c-.63-1.52-2.7-7.66.58-16,0,0,5.07-1.62,16.61,6.19a57.36,57.36,0,0,1,30.25,0C87,23.42,92.11,25,92.11,25c3.28,8.32,1.22,14.46.59,16a23.34,23.34,0,0,1,6.21,16.21c0,23.2-14.12,28.3-27.57,29.8,2.16,1.87,4.09,5.55,4.09,11.18,0,8.08-.06,14.59-.06,16.57,0,1.61,1.08,3.49,4.14,2.9A60.39,60.39,0,0,0,60.39,0Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M22.87,86.7c-.13.3-.6.39-1,.19s-.69-.61-.55-.91.61-.39,1-.19.69.61.54.91Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M25.32,89.43c-.29.27-.85.14-1.24-.28a.92.92,0,0,1-.17-1.25c.3-.27.84-.14,1.24.28s.47,1,.17,1.25Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M27.7,92.91c-.37.26-1,0-1.35-.52s-.37-1.18,0-1.44,1,0,1.35.51.37,1.19,0,1.45Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M31,96.27A1.13,1.13,0,0,1,29.41,96c-.53-.49-.68-1.18-.34-1.54s1-.27,1.56.23.68,1.18.33,1.54Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M35.46,98.22c-.15.47-.82.69-1.51.49s-1.13-.76-1-1.24.82-.7,1.51-.49,1.13.76,1,1.24Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M40.4,98.58c0,.5-.56.91-1.28.92s-1.3-.38-1.31-.88.56-.91,1.29-.92,1.3.39,1.3.88Z"
-                      />
-                      <path
-                        class="cls-2"
-                        d="M45,97.8c.09.49-.41,1-1.12,1.12s-1.35-.17-1.44-.66.42-1,1.12-1.12,1.35.17,1.44.66Z"
-                      />
-                    </g>
-                  </g>
-                </svg>{" "}
-              </StyledGithubIcon>
-              <div>
-                <h3>GitHub</h3>
-                <p>View all Pragma repositories.</p>
-              </div>
-            </CenterCard>
-          </Link>
-        </Row>
-        <Footer>
-          {" "}
-          <StyledFooterImage
-            alt="Docusaurus themed image"
-            sources={{
-              light: useBaseUrl("/img/background.jpg"),
-              dark: useBaseUrl("/img/background.jpg"),
-            }}
-          />
-          <h1
-            style={{
-              position: "absolute",
-              left: "0",
-              right: "0",
-              margin: "auto",
-              textAlign: "center",
-            }}
-          >
-            Get the latest news of Pragma Network
-          </h1>
-          <input style={{ width: "100%", height: "20px" }}></input>
-          <button style={{ width: "30px", height: "30px" }}></button>
-        </Footer>
-        <img
-          src="/img/prg_dark_logo.png"
-          style={{
-            width: "200px",
-            margin: "auto",
-            padding: "20px",
-            filter: "invert(1)",
-          }}
-        ></img>
+        <Footer />
       </Container>
     </Layout>
   );
