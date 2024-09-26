c86311bce62fe25a6633e3fd6c04bdeb3a8a8deb
Mattegoat <matteo.georges74@hotmail.fr>
2024-09-26 18:09:22 +0200

    fix: remove unused packages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -114,13 +114,4 @@ module.exports = {
     ],
   ],
   themes: ["docusaurus-theme-openapi-docs"], // export theme components
-  stylesheets: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous",
-    },
-  ],
 };
